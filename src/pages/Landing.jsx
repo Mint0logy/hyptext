49b02c98303fb78718e0075b758304dac81f4358
Mint0logy <kowalski.adrian.adam@outlook.com>
2025-04-18 22:15:25 +0200

    Added firebase config to project

---
+++ src/pages/Landing.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import styled from "styled-components";
+
+const Landing = () =>{
+    return (<Container>
+        Landing Page
+    </Container>);
+}
+
+export default Landing;
+
+const Container = styled.div`
+    width: 100%;
+    height: 100%;
+`
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import styled from "styled-components";
+
+const SignUp = () =>{
+    return (<Container>
+        Sign Up
+    </Container>);
+}
+
+export default SignUp;
