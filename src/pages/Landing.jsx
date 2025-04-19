feb28c7c4596111d104fa8fb9b79f0b89d931046
Mint0logy <kowalski.adrian.adam@outlook.com>
2025-04-19 23:47:31 +0200

    Started working on Landing Page, prepared Logo

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,15 +1,25 @@
 import React from "react";
 import styled from "styled-components";
+import LogoImage from "../assets/Logo.svg"
+
 
 const Landing = () =>{
     return (<Container>
-        Landing Page
+        <Logo />
     </Container>);
 }
 
 export default Landing;
 
 const Container = styled.div`
-    width: 100%;
-    height: 100%;
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+`
+
+const Logo = styled.img.attrs({src: `${LogoImage}`})`
+    max-width: 25vw;
 `
